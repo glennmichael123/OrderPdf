40eef228cb14b1deddaaf397c30a1abf16b5238d
Glenn Michael Torregosa <gtorregosa@gmail.com>
2020-10-22 12:01:58 +0800

    template change

--- resources/views/pdf/result.blade.php
+++ resources/views/pdf/result.blade.php
@@ -1,62 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<table>
-    <tbody>
-        <tr>
-            <td>
-                <span class="font-weight-bold" style="font-size: 1.5rem;"> Easy Cloud </span>
-            </td>
-            <td>
-                <div>Easy Cloud</div>
-                <div>Cebu City, Philippines 6000</div>
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><span class="font-weight-bold" style="font-size: 1.5rem;">PCR System Result</span></td>
-        </tr>
-    </tbody>
-</table>
-
-<hr style="border: 1px solid #b5baf7;"/>
-<div class="text-right">
+<div class="font-weight-bold text-center text-primary" style="font-size: 1.5rem;">PCR Test Notification</div>
+<div class="text-center text-secondary">(SARS-CoV-2)</div>
+<!-- <div class="text-right">
     Date Printed: {{ date('m/d/Y', strtotime($created_at)) }}
+</div> -->
+<div style="width: 55%; margin-left: auto; margin-right:auto; padding-top: 8rem; padding-bottom: 8rem;">
+    <table>
+        <tbody>
+            <tr>
+                <td>[Inspection Date]: </td>
+                <td class="font-weight-bold">{{ date('m/d/Y', strtotime($created_at)) }}</td>
+            </tr>
+            <tr>
+                <td>[Id]: </td>
+                <td class="font-weight-bold">{{ $order_number }}</td>
+            </tr>
+            <tr>
+                <td>[Name]: </td>
+                <td class="font-weight-bold">{{ $customer_name }}</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
-<table class="table table-bordered pt-3 pb-3">
-    <tbody>
-        <tr>
-            <td>Order No.: </td>
-            <td class="font-weight-bold">{{ $order_number }}</td>
-        </tr>
-        <tr>
-            <td>Customer: </td>
-            <td class="font-weight-bold">{{ $customer_name }}</td>
-        </tr>
-        <tr>
-            <td>Address: </td>
-            <td class="font-weight-bold">{{ $customer_address }}</td>
-        </tr>
-        <tr>
-            <td>E-mail: </td>
-            <td class="font-weight-bold">{{ $email }}</td>
-        </tr>
-    </tbody>
-</table>
-<div class="text-center font-weight-bold">
-    RESULT
-</div>
-<hr />
 
-@if (strtolower($result) == 'negative')
-<div class="text-success">
-    {{  $result }}
-</div>
-@else
-<div class="text-danger">
-    {{  $result }}
-</div>
-@endif
+<div style="padding-bottom: 5rem;">
+    @if (strtoupper($result) == 'NEGATIVE')
 
+    <div class="text-center">
+        <span class="text-primary" style="font-size: 1.5rem;">{{ strtoupper($result) }}: </span> Not Detected
+    </div>
+    @else
+    <div class="text-center">
+        <span class="text-danger" style="font-size: 1.5rem;">{{ strtoupper($result) }}: </span> Detected
+    </div>
+    @endif
+</div>
+
+<div class="text-center">
+    <h2>Logo Here</h2>
+</div>
 @endsection
 
